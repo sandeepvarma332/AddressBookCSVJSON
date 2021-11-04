3c8a9d7490594e0efaca6a90d8089f8f5a9cb959
sandeepvarma332 <ksandeepvarma99@gmail.com>
2021-11-04 23:00:16 +0530

    Adde: welcome message

---
+++ src/main/java/org/blz/addressbook/AddressBookMain.java
@@ -0,0 +1,10 @@
+package org.blz.addressbook;
+
+
+public class AddressBookMain
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "WELCOME TO ADDREES BOOK PROGRAM" );
+    }
+}
